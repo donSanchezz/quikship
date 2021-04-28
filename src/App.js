71a8ad2f0c50a79d72169d237c16105a4d0e0811
donSanchezz <donovansaunches@gmail.com>
2021-04-28 12:14:45 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/pages/HomePage/Home'
+import Footer from './components/pages/Footer/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home}></Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,55 @@
+export const homeObjOne = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Fast Delivery',
+  headline: 'Get your package within hours of ordering',
+  description:
+    'Our routes and mode of transport are delicately picked to ensure your package gets delivered within the shortest time possible',
+  imgStart: ' ',
+  img: 'images/svg-1.svg',
+  alt: 'Transport',
+  buttonLabel: 'Get Started',
+}
+
+export const homeObjTwo = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Safe Packaging',
+  headline: "We'll ensure your package is recieved damage free",
+  description:
+    'To ensure the safety of your goods, our team adds exra wraps from quality materials to strengthen the durability of your packages',
+  imgStart: 'start',
+  img: 'images/svg-2.svg',
+  alt: 'Safety',
+  buttonLabel: 'Learn more',
+}
+
+export const homeObjThree = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Secure Packaging',
+  headline: 'We ensure the integrity of all packages',
+  description:
+    'Every package that enters the warehouse is assigned a special vault, monitored by security personnels and equipment',
+  imgStart: ' ',
+  img: 'images/svg-3.svg',
+  alt: 'Safety',
+  buttonLabel: 'Learn more',
+}
+
+export const homeObjFour = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Instant Return',
+  headline: 'We offer returns on all packages',
+  description:
+    "Your package will be returned to the seller if you're disatisfied with the item you ordered",
+  imgStart: 'start',
+  img: 'images/svg-4.svg',
+  alt: 'Safety',
+  buttonLabel: 'Learn more',
+}
